7bb64ed9b8b1ed397526062b8b103de90a2446b4
acmeway <maling@acmeway.com>
2018-04-19 19:42:04 +0800

    alterView

--- MGWidget/MGWidget/MGWidget/LoaderView/MGAlterView.m
+++ MGWidget/MGWidget/MGWidget/LoaderView/MGAlterView.m
@@ -172,17 +172,11 @@
 
 @interface MGAlternationView ()
 
-//@property (nonatomic, strong) UIView *leftDot;
-//
-//@property (nonatomic, strong) UIView *middleDot;
-//
-//@property (nonatomic, strong) UIView *rightDot;
+@property (nonatomic, strong) UIView *leftDot;
 
-@property (nonatomic, strong) UILabel *leftDot;
+@property (nonatomic, strong) UIView *middleDot;
 
-@property (nonatomic, strong) UILabel *middleDot;
-
-@property (nonatomic, strong) UILabel *rightDot;
+@property (nonatomic, strong) UIView *rightDot;
 
 @property (nonatomic, strong) NSMutableArray *colors;
 
@@ -207,132 +201,114 @@
 
 - (void)showAnimationView
 {
-    CABasicAnimation *animation1 = [self animationKeyPath:@"position.x"
-                                                 position:CGPointMake(self.leftDot.layer.position.x + 20, self.centerY)
-                                                 duration:1
-                                                      key:@"animation1"
-                                               isReverses:NO
-                                               repeatTime:1];
-    [self.leftDot.layer addAnimation:animation1 forKey:animation1.keyPath];
-    
-    
-//    CABasicAnimation *animation2 = [self animationKeyPath:@"position.x"
-//                                                               position:CGPointMake(self.rightDot.layer.position.x - 20, self.centerY)
-//                                                               duration:1
-//                                                                    key:@"animation2"
-//                                                             isReverses:YES
-//                                                             repeatTime:1];
-//
-//    [self.rightDot.layer addAnimation:animation2 forKey:animation2.keyPath];
-    
+    [UIView animateWithDuration:1 animations:^{
+        
+        self.leftDot.center = CGPointMake(self.leftDot.layer.position.x + 20, self.centerY);
+        
+        self.rightDot.center = CGPointMake(self.rightDot.layer.position.x - 20, self.centerY);
+        
+    } completion:^(BOOL finished) {
+        
+        [UIView animateWithDuration:1 animations:^{
+        
+            self.rightDot.center = CGPointMake(self.rightDot.layer.position.x - 20, self.centerY);
+            
+            self.middleDot.center =  CGPointMake(self.leftDot.layer.position.x + 20, self.centerY);
+
+            
+        } completion:^(BOOL finished) {
+        
+            [UIView animateWithDuration:1 animations:^{
+
+                self.rightDot.center = CGPointMake(self.rightDot.layer.position.x + 20, self.centerY);
+
+                self.middleDot.center = CGPointMake(self.middleDot.layer.position.x - 20, self.centerY);
+
+                [self bringSubviewToFront:self.rightDot];
+
+            } completion:^(BOOL finished) {
+
+                [UIView animateWithDuration:1 animations:^{
+
+                    self.middleDot.center =  CGPointMake(self.middleDot.layer.position.x - 20, self.centerY);
+
+                    self.leftDot.center = CGPointMake(self.leftDot.layer.position.x + 20, self.centerY);
+
+                } completion:^(BOOL finished) {
+
+                    
+                    [UIView animateWithDuration:1 animations:^{
+                        
+                        self.middleDot.center =  CGPointMake(self.middleDot.layer.position.x + 20, self.centerY);
+                        
+                        self.leftDot.center = CGPointMake(self.leftDot.layer.position.x - 20, self.centerY);
+                        
+                        [self bringSubviewToFront:self.middleDot];
+                        
+                    } completion:^(BOOL finished) {
+                    
+                        [UIView animateWithDuration:1 animations:^{
+                        
+                            self.leftDot.center = CGPointMake(self.leftDot.layer.position.x - 20, self.centerY);
+                            
+                            self.rightDot.center = CGPointMake(self.rightDot.layer.position.x + 20, self.centerY);
+                            
+                        } completion:^(BOOL finished) {
+                            
+                            [self bringSubviewToFront:self.leftDot];
+                            
+                            [self showAnimationView];
+                        }];
+                        
+                    }];
+
+                }];
+                
+            }];
+        }];
+    }];
 }
 
-- (CABasicAnimation *)animationKeyPath:(NSString *)keyPath
-                              position:(CGPoint)position
-                              duration:(CFTimeInterval)duration
-                                   key:(NSString *)key
-                            isReverses:(BOOL)isReverses
-                            repeatTime:(CGFloat)repeat;
-{
-    
-    CABasicAnimation *animation   = [CABasicAnimation animationWithKeyPath:keyPath];
-    
-    animation.toValue             = @(position.x);
-    
-    animation.duration            = duration;
-    
-    animation.autoreverses        = isReverses;
-    
-    animation.repeatCount         = repeat;
-    
-    animation.additive            = NO;
-    
-    animation.cumulative          = NO;
-    
-    animation.delegate            = self;
-    
-    [animation setValue:key forKey:@"animatioin"];
-    
-    animation.timingFunction      = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
-    
-    animation.removedOnCompletion = NO;
-
-    animation.fillMode            = kCAFillModeForwards;
-    
-    
-    return animation;
-}
-
-- (void)animationDidStop:(CAAnimation *)anim finished:(BOOL)flag
-{
-    
-    
-    if ([[anim valueForKey:@"animatioin"] isEqualToString:@"animation1"]) {
-        
-        DLog(@"动画结束了1");
-        
-        CABasicAnimation *animation1 = [self animationKeyPath:@"position.x"
-                                                     position:CGPointMake(self.leftDot.layer.position.x - 20, self.centerY)
-                                                     duration:1
-                                                          key:@"animation2"
-                                                   isReverses:NO
-                                                   repeatTime:1];
-        [self.leftDot.layer addAnimation:animation1 forKey:animation1.keyPath];
-     
-        
-    }
-    else if ([[anim valueForKey:@"animatioin"] isEqualToString:@"animation2"])
-    {
-        DLog(@"动画结束了2");
-    }
-}
-
-- (UILabel *)leftDot
+- (UIView *)leftDot
 {
     if (!_leftDot) {
-        _leftDot                     = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 10, 10)];
-        
-        _leftDot.text = @"1";
-        
+        _leftDot                     = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 10, 10)];
+
         _leftDot.centerY             = self.centerY;
-        
+
         _leftDot.centerX             = self.centerX - 20;
-        
+
         _leftDot.layer.cornerRadius  = 5;
-        
+
         _leftDot.layer.masksToBounds = YES;
-        
+
         _leftDot.backgroundColor     = self.colors[0];
     }
     return _leftDot;
 }
 
-- (UILabel *)middleDot
+- (UIView *)middleDot
 {
     if (!_middleDot) {
-        _middleDot                     = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 10, 10)];
-        
-        _middleDot.text = @"2";
-        
+        _middleDot                     = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 10, 10)];
+
         _middleDot.centerY             = self.centerY;
-        
+
         _middleDot.centerX             = self.centerX;
-        
+
         _middleDot.layer.cornerRadius  = 5;
-        
+
         _middleDot.layer.masksToBounds = YES;
-        
+
         _middleDot.backgroundColor     = self.colors[1];
     }
     return _middleDot;
 }
 
-- (UILabel *)rightDot
+- (UIView *)rightDot
 {
     if (!_rightDot) {
-        _rightDot                     = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 10, 10)];
-
-        _rightDot.text                = @"3";
+        _rightDot                     = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 10, 10)];
 
         _rightDot.centerY             = self.centerY;
 
@@ -347,59 +323,6 @@
     return _rightDot;
 }
 
-//- (UIView *)leftDot
-//{
-//    if (!_leftDot) {
-//        _leftDot                     = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 10, 10)];
-//
-//        _leftDot.centerY             = self.centerY;
-//
-//        _leftDot.centerX             = self.centerX - 20;
-//
-//        _leftDot.layer.cornerRadius  = 5;
-//
-//        _leftDot.layer.masksToBounds = YES;
-//
-//        _leftDot.backgroundColor     = self.colors[0];
-//    }
-//    return _leftDot;
-//}
-//
-//- (UIView *)middleDot
-//{
-//    if (!_middleDot) {
-//        _middleDot                     = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 10, 10)];
-//
-//        _middleDot.centerY             = self.centerY;
-//
-//        _middleDot.centerX             = self.centerX;
-//
-//        _middleDot.layer.cornerRadius  = 5;
-//
-//        _middleDot.layer.masksToBounds = YES;
-//
-//        _middleDot.backgroundColor     = self.colors[1];
-//    }
-//    return _middleDot;
-//}
-//
-//- (UIView *)rightDot
-//{
-//    if (!_rightDot) {
-//        _rightDot                     = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 10, 10)];
-//
-//        _rightDot.centerY             = self.centerY;
-//
-//        _rightDot.centerX             = self.centerX + 20;
-//
-//        _rightDot.layer.cornerRadius  = 5;
-//
-//        _rightDot.layer.masksToBounds = YES;
-//
-//        _rightDot.backgroundColor     = self.colors[2];
-//    }
-//    return _rightDot;
-//}
 @end
 
 
